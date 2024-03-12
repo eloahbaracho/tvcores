74285f7ead2821747d85e22deec85c15d76da1bb
eloahbaracho <eloahbarachodossantos@gmail.com>
2024-03-12 09:24:03 -0300

    commit 01

---
+++ app/src/main/java/com/example/tvcores/ActivityTv.java
@@ -0,0 +1,13 @@
+package com.example.tvcores;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTv extends AppCompatActivity {
+    @Override
+    public void onCreate (Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.layout_tv);
+    }
+}
